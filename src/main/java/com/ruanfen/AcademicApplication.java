285a61a3a54a2a4ee24445a591eed530311eceed
zmk_version1 <2381854695@qq.com>
2024-11-13 20:15:08 +0800

    try

---
+++ src/main/java/com/ruanfen/AcademicApplication.java
@@ -0,0 +1,14 @@
+package com.ruanfen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AcademicApplication
+{
+    public static void main( String[] args ){
+        SpringApplication.run(AcademicApplication.class, args);
+    }
+
+}
