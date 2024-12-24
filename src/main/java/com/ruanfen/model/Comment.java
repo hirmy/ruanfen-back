8d05130584ad92161868316f228efc3187e0a526
zmk_version1 <2381854695@qq.com>
2024-12-24 22:31:59 +0800

    comment_change

--- src/main/java/com/ruanfen/model/Comment.java
+++ src/main/java/com/ruanfen/model/Comment.java
@@ -2,9 +2,11 @@ package com.ruanfen.model;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import lombok.Data;
 
 import java.time.LocalDateTime;
 
+@Data
 public class Comment {
     @TableId(value = "comment_id", type = IdType.AUTO) // 主键自增
     private Integer commentId;
